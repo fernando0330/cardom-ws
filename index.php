540985c84921ba814eb6c43f65d81947a9671338
Fernando Perez <fernandoperez0330@gmail.com>
2016-03-28 09:18:33 -0400

    test

--- index.php
+++ index.php
@@ -18,20 +18,26 @@ $app = new \Slim\Slim();
 $body = $app->request->getBody();
 $param = json_decode($body,true);
 
+header("Content-Type: image/jpeg");
+$qrCode = new QrCode();
+$qrCode
+    ->setText("Life is too short to be generating QR codes")
+    ->setSize(300)
+    ->setPadding(10)
+    ->setErrorCorrection('high')
+    ->setForegroundColor(array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0))
+    ->setBackgroundColor(array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0))
+    ->setLabel('My label')
+    ->setLabelFontSize(16)
+    ->render()
+;
+die;
+
+
+
+
 $app->get("/",function() use($param,$app) {
-    header("Content-Type: image/jpeg");
-    $qrCode = new QrCode();
-    $qrCode
-        ->setText("Life is too short to be generating QR codes")
-        ->setSize(300)
-        ->setPadding(10)
-        ->setErrorCorrection('high')
-        ->setForegroundColor(array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0))
-        ->setBackgroundColor(array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0))
-        ->setLabel('My label')
-        ->setLabelFontSize(16)
-        ->render()
-    ;
+
 });
 
 
